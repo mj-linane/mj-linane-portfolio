b6d2196e0b7a6fcb1bf5ad10e1f403d05dcc64ad
MJ Linane <19750620+mj-linane@users.noreply.github.com>
2021-12-11 18:19:31 -0500

    initial

---
+++ pages/work.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import ContainerBlock from "@components/ContainerBlock";
+import Work from "@components/Work";
+
+// noinspection JSUnusedGlobalSymbols
+export default function work(){
+    return(
+        <ContainerBlock title="Work - MJ Linane">
+            <Work/>
+        </ContainerBlock>
+    )
+}
