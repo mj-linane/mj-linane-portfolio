2b947c2b723cfdd1ceae73569b9d2fec9a09e606
MJ Linane <19750620+mj-linane@users.noreply.github.com>
2021-12-11 18:20:28 -0500

    initial

---
+++ pages/experience.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ContainerBlock from "@components/ContainerBlock";
+import Experience from "@components/Experience";
+
+// noinspection JSUnusedGlobalSymbols
+export default function experience() {
+    return (
+        <ContainerBlock title="Experience - MJ Linane">
+            <Experience/>
+        </ContainerBlock>
+    )
+}
