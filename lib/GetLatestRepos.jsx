abc8069c6d662ff9802df4cb2c91454dc53995ef
MJ Linane <19750620+mj-linane@users.noreply.github.com>
2022-01-16 15:39:04 -0500

    initial

---
+++ lib/GetLatestRepos.jsx
@@ -0,0 +1,20 @@
+import axios from "axios"
+
+const getLatestRepos = async (data) => {
+    let userName, repos
+
+    try {
+        userName = data.githubUsername
+
+        const res = await axios.get(`https://api.github.com/search/repositories?q=user:${userName}+sort:author-date-asc`)
+
+        repos = res.data.items;
+
+        } catch (err) {
+            console.log(err)
+        }
+
+    return repos.splice(0, 6)
+}
+
+export default getLatestRepos
---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+// tailwind.config.js
+module.exports = {
+    mode: "jit",
+    purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+    darkMode: "class", // or 'media' or 'class'
+    theme: {
+        extend: {
+            transitionDuration: {
+                0: "0ms",
+                2000: "2000ms",
+                4000: "4000ms",
+            },
+        },
+        fontFamily:{
+            'typewriter': ['"Special Elite"', 'monospace'],
+
+        }
+
+    },
+    variants: {
+        extend: {},
+        display: ["responsive", "group-hover", "group-focus"],
+    },
+    plugins: [
+        require("@tailwindcss/forms")({
+            strategy: 'class',
+        }),
+    ],
+};
