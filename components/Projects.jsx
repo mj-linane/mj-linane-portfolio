b531ebf98db4e67838266c6b95a2d9ceb0f37d73
MJ Linane <19750620+mj-linane@users.noreply.github.com>
2022-01-16 19:04:27 -0500

    Fix: Bug in counting and formatting of numbers and title

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -45,17 +45,16 @@ const ProjectCard = ({title, link, imgUrl, number}) => {
                     <Image
                         src={imgUrl}
                         alt="portfolio item"
-                        className="transform hover:scale-125 transition duration-2000 ease-out object-cover h-full w-full"
                         layout="fill"
                         quality={100}
                     >
                     </Image>
                 </div>
-                <h1 className="absolute top-10 left-10 font-bold text-xl bg-purple-500 px-2">
+                <h1 className="absolute top-10 left-10 font-bold text-xl font-typewriter bg-purple-500 px-2">
                     {title}
                 </h1>
-                <h1 className="dark:text-neutral-800 absolute bottom-10 left-10 font-bold text-xl">
-                    {number.length === 1 ? `0${number}` : number}
+                <h1 className="dark:text-neutral-800 absolute bottom-10 left-10 font-typewriter font-bold text-xl">
+                    {number < 10 ? `0${number}` : number}
                 </h1>
             </div>
         </a>
