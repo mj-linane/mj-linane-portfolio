096fac16a20e34af78d46b5079dad5eb5e3fbbc7
MJ Linane <19750620+mj-linane@users.noreply.github.com>
2023-05-20 14:38:05 -0400

    initial

---
+++ components/Container.tsx
@@ -0,0 +1,8 @@
+type Props = {
+    children?: React.ReactNode
+}
+
+const Container = ({ children }: Props) =>
+    <div className="container mx-auto px-5">{children}</div>
+
+export default Container
