b4c39628f6398e6ca9638ce52f9a7686d2ad648a
MJ Linane <19750620+mj-linane@users.noreply.github.com>
2023-05-20 14:36:19 -0400

    initial

---
+++ interfaces/post.ts
@@ -0,0 +1,16 @@
+import Author from "@interfaces/author";
+
+type PostType = {
+  author: Author;
+  slug: string
+  title: string
+  date: string
+  coverImage: string
+  excerpt: string
+  ogImage: {
+    url: string
+  }
+  content: string
+}
+
+export default PostType
