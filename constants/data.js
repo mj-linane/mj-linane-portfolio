4c65f13eff3fcccc32ec4894ddf784def50c6f87
MJ Linane <19750620+mj-linane@users.noreply.github.com>
2023-06-06 13:49:07 -0400

    reformat and clean up code

--- constants/data.js
+++ constants/data.js
@@ -1,136 +1,140 @@
-const userData = {
-    githubUsername: "mj-linane",
-    name: "MJ Linane",
-    designation: "Full-Stack Developer",
-    avatarUrl: "/mj-portrait.jpg",
-    email: "michael.linanejr@gmail.com",
-    address: "Rochester, MA, USA",
-    projects: [
-        {
-            title: "Portfolio",
-            link: "https://mjlinane.com",
-            imgUrl: "/mj-linane-portfolio-screenshot.jpeg",
-            favorite: false
-        },
-        {
-            title: "Real Time Chat Website",
-            link: "https://github.com/mj-linane/real-time-chat-website",
-            imgUrl: "/chat-app-repository-image.jpg",
-            favorite: true
-        },
-        {
-            title: "Text Case Converter",
-            link: "https://github.com/mj-linane/case-converter",
-            imgUrl: "/case-converter-screenshot.jpg",
-            favorite: true
-        },
-        {
-            title: "Math Wars",
-            link: "https://github.com/mj-linane/math-wars",
-            imgUrl: "/math-wars-screenshot.jpeg",
-            favorite: true
-        },
-        {
-            title: "My Coding Class",
-            link: "https://mycodingclass.co",
-            imgUrl: "/my-coding-class-screenshot.jpeg",
-            favorite: false
-        },
-        {
-            title: "Guildway",
-            link: "https://guildway.com",
-            imgUrl: "/guildway-screenshot.jpeg",
-            favorite: false
-        },
-    ],
-    about: {
-        title:
-            "I am a front-end developer that builds effective digital learning" +
-            " experiences and products.",
-        description: [
-            `I've been developing full-stack applications since 2014. I got started with Wordpress designing and building single pages (landing, contact, product launch) and customizing themes and wiring up API services like Stripe. I like to think that I was doing JAMstack before it was cool. My main focus was building learning experiences and platforms for K12 teachers and students.`,
-            `I have since moved on from Wordpress, and have been working with front-end frameworks React.js and Vue.js. I like to design my ideas in Figma and then use UI libraries Bootstrap and Tailwind to build out the products.`,
-            ` For the backend, I have experience using a number of different languages and frameworks such as, JavaScript/Express, Python/Django, C# and Java.`,
-            `I have come a long way from my first web development experiences of crashing live sites as I updated them to now building applications that are modern, scalable, performant, and maintainable.`
-        ],
-        currentProject: "My Portfolio",
-        currentProjectUrl: "https://mjlinane.com",
-        techStack: [
-            {
-                name: "JavaScript",
-                image: "/javascript.svg"
-            },
-            {
-                name: "React",
-                image: "/reactjs.svg"
-            },
-            {
-                name: "NextJS",
-                image: "/nextjs.svg"
-            },
-            {
-                name: "Express",
-                image: "/expressjs.svg"
-            },
-            {
-                name: "Python",
-                image: "/python.svg"
-            },
-            {
-                name: "Java",
-                image: "/java.svg"
-            },
-        ]
-    },
-    experience: [
-        {
-            title: "Founder and Developer",
-            company: "MyCodingClass",
-            year: "2019",
-            companyLink: "https://mycodingclass.co",
-            description: "Developed an interactive learning platform for high school" +
-                " students to learn" +
-                " computer science.",
-        },
-        {
-            title: "Computer Science Teacher",
-            company: "Old Rochester Regional High School",
-            year: "2018",
-            companyLink: "https://oldrochester.org",
-            description: "Build the computer science curriculum from scratch."
-        },
-        {
-            title: "Computer Science and Engineering Teacher",
-            company: "Old Rochester Regional High School",
-            year: "2017",
-            companyLink: "https://oldrochester.org",
-            description: "Build courses covering varying technologies such as 3D" +
-                " modeling, robotics engineering and programming, Adobe Photoshop" +
-                " and Illustrator, programming virtual reality in Python and" +
-                " JavaScript."
-        },
-        {
-            title: "Masters of Arts",
-            company: "University of Massachusetts, Boston, MA",
-            year: "2011",
-            companyLink: "https://umb.edu",
-            description: "Masters of Arts in History"
-        },
-        {
-            title: "Bachelors of Arts",
-            company: "Salem State University, Salem, MA",
-            year: "2007",
-            companyLink: "https://salemstate.edu",
-            description: "Bachelors of Arts in History"
-        }
-    ],
-    resumeUrl:
-        "https://drive.google.com/file/d/1xmE3BOmgM7TAOOgVp36xQIQvYDntDYoo/view?usp=sharing",
-    socialLinks: {
-        twitter: "https://twitter.com/mj_linane",
-        linkedin: "https://www.linkedin.com/in/mjlinane/",
-        github: "https://github.com/mj-linane",
-    },
-};
+// noinspection SpellCheckingInspection
 
-export default userData;
+const userData = {
+  githubUsername: 'mj-linane',
+  name: 'MJ Linane',
+  designation: 'Full-Stack Developer',
+  avatarUrl: '/mj-portrait.jpg',
+  email: 'michael.linanejr@gmail.com',
+  address: 'Rochester, MA, USA',
+  projects: [
+    {
+      title: 'Portfolio',
+      link: 'https://mjlinane.com',
+      imgUrl: '/mj-linane-portfolio-screenshot.jpeg',
+      favorite: false,
+    },
+    {
+      title: 'Real Time Chat Website',
+      link: 'https://github.com/mj-linane/real-time-chat-website',
+      imgUrl: '/chat-app-repository-image.jpg',
+      favorite: true,
+    },
+    {
+      title: 'Text Case Converter',
+      link: 'https://github.com/mj-linane/case-converter',
+      imgUrl: '/case-converter-screenshot.jpg',
+      favorite: true,
+    },
+    {
+      title: 'Math Wars',
+      link: 'https://github.com/mj-linane/math-wars',
+      imgUrl: '/math-wars-screenshot.jpeg',
+      favorite: true,
+    },
+    {
+      title: 'My Coding Class',
+      link: 'https://mycodingclass.co',
+      imgUrl: '/my-coding-class-screenshot.jpeg',
+      favorite: false,
+    },
+    {
+      title: 'Guildway',
+      link: 'https://guildway.com',
+      imgUrl: '/guildway-screenshot.jpeg',
+      favorite: false,
+    },
+  ],
+  about: {
+    title:
+      'I am a front-end developer that builds effective digital learning' +
+      ' experiences and products.',
+    description: [
+      `I've been developing full-stack applications since 2014. I got started with Wordpress designing and building single pages (landing, contact, product launch) and customizing themes and wiring up API services like Stripe. I like to think that I was doing JAMstack before it was cool. My main focus was building learning experiences and platforms for K12 teachers and students.`,
+      `I have since moved on from Wordpress, and have been working with front-end frameworks React.js and Vue.js. I like to design my ideas in Figma and then use UI libraries Bootstrap and Tailwind to build out the products.`,
+      ` For the backend, I have experience using a number of different languages and frameworks such as, JavaScript/Express, Python/Django, C# and Java.`,
+      `I have come a long way from my first web development experiences of crashing live sites as I updated them to now building applications that are modern, scalable, performant, and maintainable.`,
+    ],
+    currentProject: 'My Portfolio',
+    currentProjectUrl: 'https://mjlinane.com',
+    techStack: [
+      {
+        name: 'JavaScript',
+        image: '/javascript.svg',
+      },
+      {
+        name: 'React',
+        image: '/reactjs.svg',
+      },
+      {
+        name: 'NextJS',
+        image: '/nextjs.svg',
+      },
+      {
+        name: 'Express',
+        image: '/expressjs.svg',
+      },
+      {
+        name: 'Python',
+        image: '/python.svg',
+      },
+      {
+        name: 'Java',
+        image: '/java.svg',
+      },
+    ],
+  },
+  experience: [
+    {
+      title: 'Founder and Developer',
+      company: 'MyCodingClass',
+      year: '2019',
+      companyLink: 'https://mycodingclass.co',
+      description:
+        'Developed an interactive learning platform for high school' +
+        ' students to learn' +
+        ' computer science.',
+    },
+    {
+      title: 'Computer Science Teacher',
+      company: 'Old Rochester Regional High School',
+      year: '2018',
+      companyLink: 'https://oldrochester.org',
+      description: 'Build the computer science curriculum from scratch.',
+    },
+    {
+      title: 'Computer Science and Engineering Teacher',
+      company: 'Old Rochester Regional High School',
+      year: '2017',
+      companyLink: 'https://oldrochester.org',
+      description:
+        'Build courses covering varying technologies such as 3D' +
+        ' modeling, robotics engineering and programming, Adobe Photoshop' +
+        ' and Illustrator, programming virtual reality in Python and' +
+        ' JavaScript.',
+    },
+    {
+      title: 'Masters of Arts',
+      company: 'University of Massachusetts, Boston, MA',
+      year: '2011',
+      companyLink: 'https://umb.edu',
+      description: 'Masters of Arts in History',
+    },
+    {
+      title: 'Bachelors of Arts',
+      company: 'Salem State University, Salem, MA',
+      year: '2007',
+      companyLink: 'https://salemstate.edu',
+      description: 'Bachelors of Arts in History',
+    },
+  ],
+  resumeUrl:
+    'https://drive.google.com/file/d/1xmE3BOmgM7TAOOgVp36xQIQvYDntDYoo/view?usp=sharing',
+  socialLinks: {
+    twitter: 'https://twitter.com/mj_linane',
+    linkedin: 'https://www.linkedin.com/in/mjlinane/',
+    github: 'https://github.com/mj-linane',
+  },
+}
+
+export default userData
