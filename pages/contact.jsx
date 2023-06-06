dbabee91df587d43ad6ef848ba9d4102bd4a01be
MJ Linane <19750620+mj-linane@users.noreply.github.com>
2023-06-06 13:48:06 -0400

    reformat and clean up file

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,8 +1,6 @@
-import React from "react";
 import ContainerBlock from "@components/ContainerBlock";
 import Contact from "@components/Contact";
 
-// noinspection JSUnusedGlobalSymbols
 export default function contact() {
     return (
         <ContainerBlock title="Contact - MJ Linane">
