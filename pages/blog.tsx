d8ea9fd32217dbf15f13c9ee9b263699ecd44351
MJ Linane <19750620+mj-linane@users.noreply.github.com>
2023-06-06 13:41:52 -0400

    convert to typescript

---
+++ pages/blog.tsx
@@ -0,0 +1,55 @@
+import Container from '@components/Container'
+import MoreStories from '@components/MoreStories'
+import HeroPost from '@components/HeroPost'
+import Intro from '@components/Intro'
+import Layout from '@components/Layout'
+import { getAllPosts } from '@lib/api'
+import Head from 'next/head'
+import Post from '@interfaces/post'
+
+type Props = {
+  allPosts: Post[]
+}
+
+export default function Index({ allPosts }: Props) {
+  const heroPost = allPosts[0]
+  const morePosts = allPosts.slice(1)
+  return (
+    <>
+      <Layout>
+        <Head>
+          <title>my blog</title>
+        </Head>
+        <Container>
+          <Intro />
+          {heroPost && (
+            <HeroPost
+              title={heroPost.title}
+              coverImage={heroPost.coverImage}
+              date={heroPost.date}
+              author={heroPost.author}
+              slug={heroPost.slug}
+              excerpt={heroPost.excerpt}
+            />
+          )}
+          {morePosts.length > 0 && <MoreStories posts={morePosts} />}
+        </Container>
+      </Layout>
+    </>
+  )
+}
+
+export const getStaticProps = async () => {
+  const allPosts = getAllPosts([
+    'title',
+    'date',
+    'slug',
+    'author',
+    'coverImage',
+    'excerpt',
+  ])
+
+  return {
+    props: { allPosts },
+  }
+}
