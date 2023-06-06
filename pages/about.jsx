5ad4087b55cb13ae981942e211f1ac101adfcdfb
MJ Linane <19750620+mj-linane@users.noreply.github.com>
2023-06-06 13:48:19 -0400

    reformat and clean up file

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,12 +1,10 @@
-import React from "react";
-import ContainerBlock from "@components/ContainerBlock";
-import AboutMe from "@components/About";
+import ContainerBlock from '@components/ContainerBlock'
+import AboutMe from '@components/About'
 
-// noinspection JSUnusedGlobalSymbols
 export default function about() {
-    return (
-        <ContainerBlock title="About - MJ Linane">
-            <AboutMe/>
-        </ContainerBlock>
-    )
+  return (
+    <ContainerBlock title="About - MJ Linane">
+      <AboutMe />
+    </ContainerBlock>
+  )
 }
