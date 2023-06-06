3a135bae8178c59a750583f1c96f14f8dffe4a29
MJ Linane <19750620+mj-linane@users.noreply.github.com>
2023-06-06 13:44:53 -0400

    reformat and clean up file

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,25 +1,22 @@
-import React from "react";
-import userData from "@constants/data";
-import Image from "next/image";
-import { RoughNotationGroup } from "react-rough-notation";
-import RainbowHighlight from "@components/RainbowHighlight";
+import { RoughNotationGroup } from 'react-rough-notation'
+import RainbowHighlight from '@components/RainbowHighlight'
 
 export default function Hero() {
   return (
-    <section className='flex flex-row justify-center items-center'>
+    <section className="flex flex-row justify-center items-center">
       {/*Hero Text Container*/}
-      <div className='flex flex-col justify-center items-center'>
-        <div className='lg:w-1/2'>
+      <div className="flex flex-col justify-center items-center">
+        <div className="lg:w-1/2">
           <RoughNotationGroup show={true}>
-            <RainbowHighlight color={"#a855f7"}>
-              <h1 className='font-typewriter tracking-wider text-8xl md:text-6xl text-center font-bold my-10 uppercase py-7 px-4'>
+            <RainbowHighlight color={'#a855f7'}>
+              <h1 className="font-typewriter tracking-wider text-8xl md:text-6xl text-center font-bold my-10 uppercase py-7 px-4">
                 hi, my name is MJ.
               </h1>
             </RainbowHighlight>
           </RoughNotationGroup>
         </div>
-        <div className='w-3/4'>
-          <h1 className='font-typewriter tracking-wider text-center text-4xl md:text-4xl font-bold mt-20 mb-40'>
+        <div className="w-3/4">
+          <h1 className="font-typewriter tracking-wider text-center text-4xl md:text-4xl font-bold mt-20 mb-40">
             I am a front-end developer that builds effective digital learning
             experiences and products.
           </h1>
@@ -40,5 +37,5 @@ export default function Hero() {
       {/*        />*/}
       {/*</div>*/}
     </section>
-  );
+  )
 }
