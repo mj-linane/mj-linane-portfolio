a3c408fabc9420c193606472206aea0d8330f088
MJ Linane <19750620+mj-linane@users.noreply.github.com>
2023-06-06 13:45:23 -0400

    reformatted and cleaned up code

--- components/Experience.jsx
+++ components/Experience.jsx
@@ -1,7 +1,7 @@
-import React from "react";
 import userData from "@constants/data";
 import { RoughNotationGroup } from "react-rough-notation";
-import RainbowHighlight from "@components/RainbowHighlight";
+import RainbowHighlight from "./RainbowHighlight";
+import ExperienceCard from "./ExperienceCard";
 
 export default function Experience() {
   return (
@@ -48,22 +48,3 @@ export default function Experience() {
     </section>
   );
 }
-
-const ExperienceCard = ({ title, description, year, company, companyLink }) => {
-  return (
-    <div className='relative border-4 border-black box-shadow-md-black p-4 bg-neutral-50 dark:bg-neutral-800 z-10 mx-4'>
-      <h1 className='absolute -top-10 lg:top-15 font-typewriter tracking-wider text-4xl text-neutral-500 font-bold dark:text-neutral-400'>
-        {year}
-      </h1>
-      <h1 className='font-semibold font-typewriter tracking-wider text-xl'>
-        {title}
-      </h1>
-      <a href={companyLink} className='text-neutral-500'>
-        {company}
-      </a>
-      <p className='text-neutral-600 dark:text-neutral-400 my-2'>
-        {description}
-      </p>
-    </div>
-  );
-};
