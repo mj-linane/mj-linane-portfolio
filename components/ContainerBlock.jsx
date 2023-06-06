d3abd93e65d5cb98b3ee6711b0f2ff9af2da2ca2
MJ Linane <19750620+mj-linane@users.noreply.github.com>
2023-06-06 13:43:21 -0400

    clean up file

--- components/ContainerBlock.jsx
+++ components/ContainerBlock.jsx
@@ -1,54 +1,47 @@
-import Head from "next/head";
-import {useRouter} from 'next/router';
+import Head from 'next/head'
+import { useRouter } from 'next/router'
 import NavBar from './NavBar.jsx'
 import Footer from './Footer.jsx'
 
-export default function ContainerBlock({children, ...customMeta}) {
-    const router = useRouter();
+export default function ContainerBlock({ children, ...customMeta }) {
+  const router = useRouter()
 
-    const meta = {
-        title: "MJ Linane - Developer, Designer, Teacher",
-        description: "I have been developing websites and web applications" +
-            " for over 7 years. Get in touch with me to learn more.",
-        image: "/avatar.png",
-        type: "website",
-        ...customMeta
-    };
+  const meta = {
+    title: 'MJ Linane - Developer, Designer, Teacher',
+    description:
+      'I have been developing websites and web applications' +
+      ' for over 7 years. Get in touch with me to learn more.',
+    image: '/avatar.png',
+    type: 'website',
+    ...customMeta,
+  }
 
-    return (
-        <div>
-            <Head>
-                <title>{meta.title}</title>
-                <meta name="robots"
-                      content="follow, index"/>
-                <meta content={meta.description}
-                      name="description"/>
-                <meta property="og:url"
-                      content={`https://mjlinane.com${router.asPath}`}/>
-                <link rel="canonical"
-                      href={`https://mjlinane.com${router.asPath}`}/>
-                <meta property="og:type"
-                      content={meta.type}/>
-                <meta property="og:image"
-                      content={meta.image}/>
-                <meta property="twitter:card"
-                      content="summary_large_image"/>
-                <meta property="twitter:title"
-                      content="@mjlinane"/>
-                <meta property="twitter:description"
-                      content={meta.description}/>
-                <meta property="twitter:image"
-                      content={meta.image}/>
-                {meta.date && (
-                    <meta property="article:published_time"
-                          content={meta.date}/>
-                )}
-            </Head>
-            <main className="bg-neutral-50 dark:bg-neutral-800 text-neutral-800 dark:text-neutral-50 w-full">
-                <NavBar/>
-                {children}
-                <Footer/>
-            </main>
-        </div>
-    )
+  return (
+    <div>
+      <Head>
+        <title>{meta.title}</title>
+        <meta name="robots" content="follow, index" />
+        <meta content={meta.description} name="description" />
+        <meta
+          property="og:url"
+          content={`https://mjlinane.com${router.asPath}`}
+        />
+        <link rel="canonical" href={`https://mjlinane.com${router.asPath}`} />
+        <meta property="og:type" content={meta.type} />
+        <meta property="og:image" content={meta.image} />
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:title" content="@mjlinane" />
+        <meta property="twitter:description" content={meta.description} />
+        <meta property="twitter:image" content={meta.image} />
+        {meta.date && (
+          <meta property="article:published_time" content={meta.date} />
+        )}
+      </Head>
+      <main className="bg-neutral-50 dark:bg-neutral-800 text-neutral-800 dark:text-neutral-50 w-full">
+        <NavBar />
+        {children}
+        <Footer />
+      </main>
+    </div>
+  )
 }
