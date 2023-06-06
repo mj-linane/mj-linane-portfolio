25c2746f63b6821a2814a7f31883306b1c848c0a
MJ Linane <19750620+mj-linane@users.noreply.github.com>
2023-06-06 13:48:11 -0400

    reformat and clean up file

--- pages/experience.jsx
+++ pages/experience.jsx
@@ -1,12 +1,10 @@
-import React from "react";
-import ContainerBlock from "@components/ContainerBlock";
-import Experience from "@components/Experience";
+import ContainerBlock from '@components/ContainerBlock'
+import Experience from '@components/Experience'
 
-// noinspection JSUnusedGlobalSymbols
 export default function experience() {
-    return (
-        <ContainerBlock title="Experience - MJ Linane">
-            <Experience/>
-        </ContainerBlock>
-    )
+  return (
+    <ContainerBlock title="Experience - MJ Linane">
+      <Experience />
+    </ContainerBlock>
+  )
 }
