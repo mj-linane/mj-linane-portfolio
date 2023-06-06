fefa1159fa78b1b79cfb05af042d1001348b5c60
MJ Linane <19750620+mj-linane@users.noreply.github.com>
2023-06-06 13:31:41 -0400

    convert to typescript

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,11 @@
 import '../styles/globals.css'
 import {ThemeProvider} from "next-themes"
+import { AppProps } from 'next/app'
 
-function MyApp({Component, pageProps}) {
+export default function MyApp({ Component, pageProps }: AppProps) {
     return (
         <ThemeProvider defaultTheme="light" attribute="class">
             <Component{...pageProps} />
         </ThemeProvider>
     )
 }
-
-// noinspection JSUnusedGlobalSymbols
-export default MyApp
