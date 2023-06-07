78b730d50c483a4578ee9d9a1659b4f2a681c92f
MJ Linane <19750620+mj-linane@users.noreply.github.com>
2023-06-06 22:38:43 -0400

    fixed eslint problems and clean up

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -1,32 +1,36 @@
-type ExperienceCard = {
-  title: string;
-  description: string;
-  year: string;
-  company: string;
-  companyLink: string;
-};
-
 type Props = {
-  experienceCard: ExperienceCard;
+    title: string;
+    description: string;
+    year: string;
+    company: string;
+    companyLink: string;
 };
 
-const ExperienceCard = ({ experienceCard }: Props) =>
+const ExperienceCard = ( experience : Props ) =>
     (
         <div className='relative border-4 border-black box-shadow-md-black p-4 bg-neutral-50 dark:bg-neutral-800 z-10 mx-4'>
             <h1 className='absolute -top-10 lg:top-15 font-typewriter tracking-wider text-4xl text-neutral-500 font-bold dark:text-neutral-400'>
-                {experienceCard.year}
+                {experience.year}
             </h1>
             <h1 className='font-semibold font-typewriter tracking-wider text-xl'>
-                {experienceCard.title}
+                {experience.title}
             </h1>
-            <a href={experienceCard.companyLink}
+            <a href={experience.companyLink}
                className='text-neutral-500'>
-                {experienceCard.company}
+                {experience.company}
             </a>
             <p className='text-neutral-600 dark:text-neutral-400 my-2'>
-                {experienceCard.description}
+                {experience.description}
             </p>
         </div>
     );
+
+ExperienceCard.defaultProps = {
+    title: 'Title',
+    description: 'Description',
+    year: 'Year',
+    company: 'Company',
+    companyLink: 'https://www.google.com',
+}
 
 export default ExperienceCard;
