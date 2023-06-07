6506ca4b05aca09af5b4eac71879ea7d461f2d21
MJ Linane <19750620+mj-linane@users.noreply.github.com>
2023-06-06 22:39:48 -0400

    fixed eslint problems

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,5 +1,5 @@
-import ContainerBlock from "@components/ContainerBlock";
 import Contact from "@components/Contact";
+import ContainerBlock from "@components/ContainerBlock";
 
 export default function contact() {
     return (
