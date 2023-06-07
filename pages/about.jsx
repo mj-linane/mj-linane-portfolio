3d566963245549661e82b7d9fb6f66e3575855c1
MJ Linane <19750620+mj-linane@users.noreply.github.com>
2023-06-06 22:39:39 -0400

    fixed eslint problems

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,5 +1,5 @@
-import ContainerBlock from '@components/ContainerBlock'
 import AboutMe from '@components/About'
+import ContainerBlock from '@components/ContainerBlock'
 
 export default function about() {
   return (
