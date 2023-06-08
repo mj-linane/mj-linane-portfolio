c5c81a679ef62d8fd28a1116b1471dd781040096
MJ Linane <19750620+mj-linane@users.noreply.github.com>
2023-06-08 12:23:53 -0400

    update imports

--- components/HeroPost.tsx
+++ components/HeroPost.tsx
@@ -1,10 +1,10 @@
 import Link from 'next/link'
 
-import type Author from '@interfaces/author'
-
 import Avatar from './Avatar'
 import CoverImage from './CoverImage'
 import DateFormatter from './DateFormatter'
+
+import type Author from '@/interfaces/author'
 
 type Props = {
   title: string
@@ -22,35 +22,32 @@ const HeroPost = ({
   excerpt,
   author,
   slug,
-}: Props) =>
-    (
-        <section>
-          <div className="mb-8 md:mb-16">
-            <CoverImage title={title}
-                        src={coverImage}
-                        slug={slug}/>
-          </div>
-          <div className="md:grid md:grid-cols-2 md:gap-x-16 lg:gap-x-8 mb-20 md:mb-28">
-            <div>
-              <h3 className="mb-4 text-4xl lg:text-5xl leading-tight">
-                <Link
-                    as={`/posts/${slug}`}
-                    href="/posts/[slug]"
-                    className="hover:underline"
-                >
-                  {title}
-                </Link>
-              </h3>
-              <div className="mb-4 md:mb-0 text-lg">
-                <DateFormatter dateString={date}/>
-              </div>
-            </div>
-            <div>
-              <p className="text-lg leading-relaxed mb-4">{excerpt}</p>
-              <Avatar name={author.name} picture={author.picture}/>
-            </div>
-          </div>
-        </section>
-    )
+}: Props) => (
+  <section>
+    <div className="mb-8 md:mb-16">
+      <CoverImage title={title} src={coverImage} slug={slug} />
+    </div>
+    <div className="md:grid md:grid-cols-2 md:gap-x-16 lg:gap-x-8 mb-20 md:mb-28">
+      <div>
+        <h3 className="mb-4 text-4xl lg:text-5xl leading-tight">
+          <Link
+            as={`/posts/${slug}`}
+            href="/posts/[slug]"
+            className="hover:underline"
+          >
+            {title}
+          </Link>
+        </h3>
+        <div className="mb-4 md:mb-0 text-lg">
+          <DateFormatter dateString={date} />
+        </div>
+      </div>
+      <div>
+        <p className="text-lg leading-relaxed mb-4">{excerpt}</p>
+        <Avatar name={author.name} picture={author.picture} />
+      </div>
+    </div>
+  </section>
+)
 
 export default HeroPost
