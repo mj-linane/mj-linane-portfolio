351d376672ac4b11be925f0842bf4c56b329fcd8
MJ Linane <19750620+mj-linane@users.noreply.github.com>
2023-06-08 12:20:25 -0400

    update formatting

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -1,21 +1,20 @@
-import Image from "next/image";
+import Image from 'next/image'
 
 type Props = {
-  name: string;
-  picture: string;
-};
+  name: string
+  picture: string
+}
 
-const Avatar = ({ name, picture }: Props) =>
-    (
-        <div className='flex items-center'>
-            <Image
-                src={picture}
-                alt={name}
-                width={48}
-                height={48}
-                className='w-12 h-12 rounded-full mr-4'
-            />
-        </div>
-    );
+const Avatar = ({ name, picture }: Props) => (
+  <div className="flex items-center">
+    <Image
+      src={picture}
+      alt={name}
+      width={48}
+      height={48}
+      className="w-12 h-12 rounded-full mr-4"
+    />
+  </div>
+)
 
-export default Avatar;
+export default Avatar
