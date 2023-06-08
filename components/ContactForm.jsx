e2e3d84db0384b3fed78931bb44ea7f83dfbea95
MJ Linane <19750620+mj-linane@users.noreply.github.com>
2023-06-08 12:20:59 -0400

    update imports

--- components/ContactForm.jsx
+++ components/ContactForm.jsx
@@ -1,64 +1,64 @@
-import { useForm, ValidationError } from "@formspree/react";
+import { useForm, ValidationError } from '@formspree/react'
 
 export default function ContactForm() {
-  const [state, handleSubmit] = useForm("mlezndgo");
+  const [state, handleSubmit] = useForm('mlezndgo')
 
   if (state.succeeded) {
     return (
-      <p className='font-typewriter text-neutral-800 font-semibold'>
+      <p className="font-typewriter text-neutral-800 font-semibold">
         Thanks! I will get back to you soon!
       </p>
-    );
+    )
   }
 
   return (
-    <form className='form p-4 flex flex-col w-full' onSubmit={handleSubmit}>
+    <form className="form p-4 flex flex-col w-full" onSubmit={handleSubmit}>
       <label
-        htmlFor='name'
-        className='form-label text-sm text-neutral-800 uppercase mx-4 track-wide'
+        htmlFor="name"
+        className="form-label text-sm text-neutral-800 uppercase mx-4 track-wide"
       >
         Your Name
       </label>
       <input
-        type='text'
-        className='form-input border-2 border-black box-shadow-black dark:bg-neutral-50 py-2 mt-2 px-1 mx-4'
-        name='name'
-        id='name'
+        type="text"
+        className="form-input border-2 border-black box-shadow-black dark:bg-neutral-50 py-2 mt-2 px-1 mx-4"
+        name="name"
+        id="name"
       />
       <label
-        htmlFor='email'
-        className='form-label text-sm text-neutral-800 uppercase mx-4 mt-4 track-wide'
+        htmlFor="email"
+        className="form-label text-sm text-neutral-800 uppercase mx-4 mt-4 track-wide"
       >
         Your Email
       </label>
       <input
-        type='email'
-        className='form-input border-2 border-black box-shadow-black dark:bg-neutral-50 py-2 mt-2 px-1 mx-4 '
-        name='email'
-        id='email'
-        typeof='email'
+        type="email"
+        className="form-input border-2 border-black box-shadow-black dark:bg-neutral-50 py-2 mt-2 px-1 mx-4 "
+        name="email"
+        id="email"
+        typeof="email"
       />
-      <ValidationError prefix='Email' field='email' errors={state.errors} />
+      <ValidationError prefix="Email" field="email" errors={state.errors} />
 
       <label
-        htmlFor='comment'
-        className='form-label text-sm text-neutral-800 uppercase mx-4 mt-4 track-wide'
+        htmlFor="comment"
+        className="form-label text-sm text-neutral-800 uppercase mx-4 mt-4 track-wide"
       >
         Your Message
       </label>
       <input
-        type='textarea'
-        className='form-textarea border-2 border-black box-shadow-black dark:bg-neutral-50 py-2 mt-2 px-1 mx-4'
-        name='comment'
+        type="textarea"
+        className="form-textarea border-2 border-black box-shadow-black dark:bg-neutral-50 py-2 mt-2 px-1 mx-4"
+        name="comment"
       />
       <button
-        type='submit'
-        className='text-sm border-2 border-black bg-purple-500 hover:bg-black text-neutral-50 cursor-pointer h-10 uppercase mx-4 mt-8 py-2 tracking-wider'
+        type="submit"
+        className="text-sm border-2 border-black bg-purple-500 hover:bg-black text-neutral-50 cursor-pointer h-10 uppercase mx-4 mt-8 py-2 tracking-wider"
         disabled={state.submitting}
       >
         Send Message
       </button>
       <ValidationError errors={state.errors} />
     </form>
-  );
+  )
 }
