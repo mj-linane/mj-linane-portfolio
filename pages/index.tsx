4300e77a91d2c63b59a24756cdf1143f8f76fc0d
MJ Linane <19750620+mj-linane@users.noreply.github.com>
2023-06-08 12:23:45 -0400

    update imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,37 @@
-import ContainerBlock from "@components/ContainerBlock";
-import FavoriteProjects from "@components/FavoriteProjects";
-import Hero from "@components/Hero";
-import LatestCode from "@components/LatestCode";
-// import {getLatestRepos} from "@lib/api";
-import userData from "@constants/data";
-import getLatestRepos from '@lib/GetLatestRepos';
+import ContainerBlock from '@/components/ContainerBlock'
+import FavoriteProjects from '@/components/FavoriteProjects'
+import Hero from '@/components/Hero'
+import LatestCode from '@/components/LatestCode'
+
+import userData from '@/constants/data'
+import getLatestRepos from '@/lib/GetLatestRepos'
 
 type Repository = {
-    id: string;
+  id: string
 }
 
 type Props = {
-    repositories: Repository[];
+  repositories: Repository[]
 }
 
-export default function Home({repositories} : Props) {
-    return (
-        <ContainerBlock
-            title="MJ Linane - Web Developer and Teacher"
-            description="This is my collection of best work.">
-            <Hero/>
-            <FavoriteProjects/>
-            <LatestCode repositories={repositories}/>
-        </ContainerBlock>
-    )
+export default function Home({ repositories }: Props) {
+  return (
+    <ContainerBlock
+      title="MJ Linane - Web Developer and Teacher"
+      description="This is my collection of best work."
+    >
+      <Hero />
+      <FavoriteProjects />
+      <LatestCode repositories={repositories} />
+    </ContainerBlock>
+  )
 }
 
 export async function getStaticProps() {
-    const repositories = await getLatestRepos(userData);
-    return {
-        props: {
-            repositories,
-        },
-    };
+  const repositories = await getLatestRepos(userData)
+  return {
+    props: {
+      repositories,
+    },
+  }
 }
