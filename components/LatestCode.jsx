bfec5cd54d9e856ee5d9898bae96d4327484a4bb
MJ Linane <19750620+mj-linane@users.noreply.github.com>
2023-06-08 12:22:24 -0400

    update imports

--- components/LatestCode.jsx
+++ components/LatestCode.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react'
 
-import LatestCodeCard from '@components/LatestCodeCard'
-import userData from '@constants/data'
-import getLatestRepos from '@lib/GetLatestRepos.js'
-// import { getLatestRepos } from "@lib/api"
+import LatestCodeCard from '@/components/LatestCodeCard'
+
+import userData from '@/constants/data'
+import getLatestRepos from '@/lib/GetLatestRepos'
 
 export default function LatestCode({ repositories }) {
   const [repos, setRepos] = useState([])
