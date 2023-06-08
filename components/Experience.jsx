3708f6768abb0c91e9a2110e52c0c726e73ea4e2
MJ Linane <19750620+mj-linane@users.noreply.github.com>
2023-06-08 12:21:54 -0400

    update imports

--- components/Experience.jsx
+++ components/Experience.jsx
@@ -1,17 +1,17 @@
-import { RoughNotationGroup } from "react-rough-notation";
+import { RoughNotationGroup } from 'react-rough-notation'
 
-import userData from "@constants/data";
+import ExperienceCard from './ExperienceCard'
+import RainbowHighlight from './RainbowHighlight'
 
-import ExperienceCard from "./ExperienceCard";
-import RainbowHighlight from "./RainbowHighlight";
+import userData from '@/constants/data'
 
 export default function Experience() {
   return (
-    <section className='bg-neutral-50 dark:bg-neutral-800'>
-      <div className='max-w-6xl mx-auto h-48'>
+    <section className="bg-neutral-50 dark:bg-neutral-800">
+      <div className="max-w-6xl mx-auto h-48">
         <RoughNotationGroup show={true}>
-          <RainbowHighlight color={"#a855f7"}>
-            <h1 className='font-typewriter font-bold text-center md:text-9xl md:text-left z-10 tracking-wider py-20'>
+          <RainbowHighlight color={'#a855f7'}>
+            <h1 className="font-typewriter font-bold text-center md:text-9xl md:text-left z-10 tracking-wider py-20">
               my experience.
             </h1>
           </RainbowHighlight>
@@ -22,8 +22,8 @@ export default function Experience() {
       {/*        Experience*/}
       {/*    </h1>*/}
       {/*</div>*/}
-      <div className='bg-neutral-200 dark:bg-neutral-900 -mt-4 py-20 lg:py-40'>
-        <div className='grid grid-cols-1 dark:bg-neutral-900 max-w-xl mx-auto pt-20'>
+      <div className="bg-neutral-200 dark:bg-neutral-900 -mt-4 py-20 lg:py-40">
+        <div className="grid grid-cols-1 dark:bg-neutral-900 max-w-xl mx-auto pt-20">
           {/* Experience Card*/}
           {userData.experience.map((experience, index) => (
             <>
@@ -36,11 +36,11 @@ export default function Experience() {
                 companyLink={experience.companyLink}
               />
               {index === userData.experience.length - 1 ? null : (
-                <div className='divider-container flex flex-col items-center -mt-2'>
-                  <div className='w-4 h-4 bg-purple-500 rounded-full relative z-10'>
-                    <div className='w-4 h-4 bg-purple-500 rounded-full relative z-10 animate-ping'></div>
+                <div className="divider-container flex flex-col items-center -mt-2">
+                  <div className="w-4 h-4 bg-purple-500 rounded-full relative z-10">
+                    <div className="w-4 h-4 bg-purple-500 rounded-full relative z-10 animate-ping"></div>
                   </div>
-                  <div className='w-1 h-24 bg-purple-500 rounded-full -mt-2 z-10'></div>
+                  <div className="w-1 h-24 bg-purple-500 rounded-full -mt-2 z-10"></div>
                 </div>
               )}
             </>
@@ -48,5 +48,5 @@ export default function Experience() {
         </div>
       </div>
     </section>
-  );
+  )
 }
