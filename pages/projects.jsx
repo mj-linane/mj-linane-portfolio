7eb0559ba00839b5c983135b74681a84fa5873ff
MJ Linane <19750620+mj-linane@users.noreply.github.com>
2023-06-08 12:23:23 -0400

    update imports

--- pages/projects.jsx
+++ pages/projects.jsx
@@ -1,13 +1,12 @@
-import React from "react";
+import React from 'react'
 
-import ContainerBlock from "@components/ContainerBlock";
-import Projects from "@components/Projects";
+import ContainerBlock from '@/components/ContainerBlock'
+import Projects from '@/components/Projects'
 
-// noinspection JSUnusedGlobalSymbols
 export default function projects() {
-    return (
-        <ContainerBlock title="Projects - MJ Linane">
-            <Projects/>
-        </ContainerBlock>
-    )
+  return (
+    <ContainerBlock title="Projects - MJ Linane">
+      <Projects />
+    </ContainerBlock>
+  )
 }
