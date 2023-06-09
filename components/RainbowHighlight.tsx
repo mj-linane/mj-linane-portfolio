19ddb63b551fb14ab31a8d5298adadde4bc6e512
mjlinane <19750620+mj-linane@users.noreply.github.com>
2023-06-09 13:51:04 -0400

    refactor to typescript

--- components/RainbowHighlight.tsx
+++ components/RainbowHighlight.tsx
@@ -1,21 +1,30 @@
 import { RoughNotation } from 'react-rough-notation'
 
-const RainbowHighlight = ({ color, children }) => {
-  // Change animation based on length of text we're animating (speed =
-  // distance / time)
+type Props = {
+  color: string
+  // children that is a react node of a string
+  children: React.ReactNode
+}
+
+const RainbowHighlight = ({ color, children }: Props) => {
+  // Change animation based on length of text we're animating (speed = distance / time)
   const animationDuration = Math.floor(30 * children.length)
   return (
     <RoughNotation
       type="highlight"
       multiline={true}
       padding={[0, 2]}
-      iteration={1}
+      iterations={1}
       animationDuration={animationDuration}
       color={color}
     >
       {children}
     </RoughNotation>
   )
+}
+
+RainbowHighlight.defaultProps = {
+  color: '#a855f7',
 }
 
 export default RainbowHighlight
