1885af60246562885cd341ffb30333c0183da65a
mjlinane <19750620+mj-linane@users.noreply.github.com>
2023-06-09 13:50:21 -0400

    refactor to typescript

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -1,6 +1,14 @@
 import Image from 'next/image'
 
-export const ProjectCard = ({ title, link, imgUrl, number }) => (
+type Props = {
+  imgUrl: string
+  altText: string
+  title: string
+  link: string
+  number: number
+}
+
+const ProjectCard = ({ title, link, imgUrl, number }: Props) => (
   <a
     href={link}
     className="bg-neutral-50 w-full block border-2 border-black box-shadow-md-black"
@@ -24,3 +32,13 @@ export const ProjectCard = ({ title, link, imgUrl, number }) => (
     </div>
   </a>
 )
+
+ProjectCard.defaultProps = {
+  imgUrl: 'https://via.placeholder.com/300x200',
+  altText: 'placeholder',
+  title: 'Project Title',
+  link: 'https://github.com',
+  number: 1,
+}
+
+export default ProjectCard
