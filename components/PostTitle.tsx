1c94692fc44a401cceec40c145a8d95a205d40d9
MJ Linane <19750620+mj-linane@users.noreply.github.com>
2023-06-12 14:31:33 -0400

    add default prop

--- components/PostTitle.tsx
+++ components/PostTitle.tsx
@@ -4,7 +4,7 @@ type Props = {
   children?: ReactNode
 }
 
-const PostTitle = ({ children }: Props) => (
+const PostTitle = ({ children = null }: Props) => (
   <h1 className="text-5xl md:text-7xl lg:text-8xl font-bold tracking-tighter leading-tight md:leading-none mb-12 text-center md:text-left">
     {children}
   </h1>
