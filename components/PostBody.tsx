17730eb6bca3be7885457290067afbf47bce5667
MJ Linane <19750620+mj-linane@users.noreply.github.com>
2023-06-12 14:31:10 -0400

    refactor to functional component

--- components/PostBody.tsx
+++ components/PostBody.tsx
@@ -2,10 +2,10 @@ type Props = {
   content: string
 }
 
-const PostBody = ({ content }: Props) => (
-  <div className="max-w-2xl mx-auto">
-    <div dangerouslySetInnerHTML={{ __html: content }} />
-  </div>
-)
-
-export default PostBody
+export default function PostBody({ content }: Props) {
+  return (
+    <div className="max-w-2xl mx-auto">
+      <div dangerouslySetInnerHTML={{ __html: content }} />
+    </div>
+  )
+}
