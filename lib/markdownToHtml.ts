6e35f3f0e8d4b82cb89ee4068af46d624733391b
MJ Linane <19750620+mj-linane@users.noreply.github.com>
2023-06-12 16:15:54 -0400

    refactor to typescript and add types

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -1,7 +1,11 @@
 import { remark } from 'remark'
-import html from 'remark-html'
+import remarkHTML from 'remark-html'
+import { Plugin } from 'unified'
+import { Node } from 'unist'
 
 export default async function markdownToHtml(markdown: string) {
-  const result = await remark().use(html).process(markdown)
+  const result = await remark()
+    .use(remarkHTML as Plugin<[], Node, Node>)
+    .process(markdown)
   return result.toString()
 }
