bd331faf10677faa8fd1ac3fd507eaa6ad9b77d3
MJ Linane <19750620+mj-linane@users.noreply.github.com>
2023-06-12 14:29:12 -0400

    deconstruct props

--- components/HeroPost.tsx
+++ components/HeroPost.tsx
@@ -14,46 +14,39 @@ type Props = {
   slug: string
 }
 
-export default function HeroPost(postData: Props) {
+export default function HeroPost({
+  title,
+  coverImage,
+  date,
+  excerpt,
+  author,
+  slug,
+}: Props) {
   return (
     <section>
       <div className="mb-8 md:mb-16">
-        <CoverImage
-          title={postData.title}
-          src={postData.coverImage}
-          slug={postData.slug}
-        />
+        <CoverImage title={title} src={coverImage} slug={slug} />
       </div>
       <div className="md:grid md:grid-cols-2 md:gap-x-16 lg:gap-x-8 mb-20 md:mb-28">
         <div>
           <h3 className="mb-4 text-4xl lg:text-5xl leading-tight">
             <Link
-              as={`/posts/${postData.slug}`}
+              as={`/posts/${slug}`}
               href="/posts/[slug]"
               className="hover:underline"
             >
-              {postData.title}
+              {title}
             </Link>
           </h3>
           <div className="mb-4 md:mb-0 text-lg">
-            <DateFormatter dateString={postData.date} />
+            <DateFormatter dateString={date} />
           </div>
         </div>
         <div>
-          <p className="text-lg leading-relaxed mb-4">{postData.excerpt}</p>
-          <Avatar
-            name={postData.author.name}
-            picture={postData.author.picture}
-          />
+          <p className="text-lg leading-relaxed mb-4">{excerpt}</p>
+          <Avatar name={author.name} picture={author.picture} />
         </div>
       </div>
     </section>
   )
-}
-
-HeroPost.defaultProps = {
-  title: 'Title',
-  coverImage: 'https://via.placeholder.com/300x200',
-  date: '2021-01-01',
-  excerpt: 'This is an excerpt',
 }
