bc419de260684b2a8919df05e21f1c44c9b07071
MJ Linane <19750620+mj-linane@users.noreply.github.com>
2023-06-12 14:25:59 -0400

    refactor default props

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -4,19 +4,18 @@ import Footer from '@/components/Footer'
 import Meta from '@/components/Meta'
 
 type Props = {
-  preview?: boolean
-  children: React.ReactNode
+  children?: React.ReactNode
 }
 
-const Layout = ({ children }: Props) => (
-  <>
-    <Meta />
-    <div className="min-h-screen">
-      {/*<Alert preview={preview}/>*/}
-      <main>{children}</main>
-    </div>
-    <Footer />
-  </>
-)
-
-export default Layout
+export default function Layout({ children = null }: Props) {
+  return (
+    <>
+      <Meta />
+      <div className="min-h-screen">
+        {/*<Alert preview={preview}/>*/}
+        <main>{children}</main>
+      </div>
+      <Footer />
+    </>
+  )
+}
