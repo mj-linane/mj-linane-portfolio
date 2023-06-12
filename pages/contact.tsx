5a30a671d48fc106116e3ed73b6b6d37d570b7b4
MJ Linane <19750620+mj-linane@users.noreply.github.com>
2023-06-12 07:56:27 -0400

    convert to typescript

---
+++ pages/contact.tsx
@@ -0,0 +1,10 @@
+import Contact from '@/components/Contact'
+import ContainerBlock from '@/components/ContainerBlock'
+
+export default function contact() {
+  return (
+    <ContainerBlock title="Contact - MJ Linane">
+      <Contact />
+    </ContainerBlock>
+  )
+}
