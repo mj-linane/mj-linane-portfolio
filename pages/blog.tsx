93f728efdf1cbeded04a56e0ef1ced0bc9d2f578
MJ Linane <19750620+mj-linane@users.noreply.github.com>
2023-06-12 14:30:40 -0400

    fix incorrect page name

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -12,7 +12,7 @@ type Props = {
   allPosts: Post[]
 }
 
-export default function Index({ allPosts }: Props) {
+export default function blog({ allPosts }: Props) {
   const heroPost = allPosts[0]
   const morePosts = allPosts.slice(1)
   return (
@@ -41,7 +41,7 @@ export default function Index({ allPosts }: Props) {
 }
 
 export const getStaticProps = async () => {
-  const allPosts = getAllPosts([
+  const allPosts = await getAllPosts([
     'title',
     'date',
     'slug',
