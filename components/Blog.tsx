434753b4bffa886d1467de3115551882eee7ab90
MJ Linane <19750620+mj-linane@users.noreply.github.com>
2023-06-14 13:41:14 -0400

    split up blog logic from rendering

---
+++ components/Blog.tsx
@@ -0,0 +1,30 @@
+import Container from '@/components/Container'
+import HeroPost from '@/components/HeroPost'
+import Intro from '@/components/Intro'
+import MoreStories from '@/components/MoreStories'
+import PostType from '@/interfaces/post'
+
+type Props = {
+  allPosts: PostType[]
+}
+
+export default function Blog({ allPosts }: Props) {
+  const heroPost = allPosts[0]
+  const morePosts = allPosts.slice(1)
+  return (
+    <Container>
+      <Intro />
+      {heroPost && (
+        <HeroPost
+          title={heroPost.title}
+          coverImage={heroPost.coverImage}
+          date={heroPost.date}
+          author={heroPost.author}
+          slug={heroPost.slug}
+          excerpt={heroPost.excerpt}
+        />
+      )}
+      {morePosts.length > 0 && <MoreStories posts={morePosts} />}
+    </Container>
+  )
+}
--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,10 +1,5 @@
-import Head from 'next/head'
-
-import Container from '@/components/Container'
-import HeroPost from '@/components/HeroPost'
-import Intro from '@/components/Intro'
-import Layout from '@/components/Layout'
-import MoreStories from '@/components/MoreStories'
+import Blog from '@/components/Blog'
+import ContainerBlock from '@/components/ContainerBlock'
 import PostType from '@/interfaces/post'
 import { getAllPosts } from '@/lib/api'
 
@@ -13,30 +8,10 @@ type Props = {
 }
 
 export default function blog({ allPosts }: Props) {
-  const heroPost = allPosts[0]
-  const morePosts = allPosts.slice(1)
   return (
-    <>
-      <Layout>
-        <Head>
-          <title>my blog</title>
-        </Head>
-        <Container>
-          <Intro />
-          {heroPost && (
-            <HeroPost
-              title={heroPost.title}
-              coverImage={heroPost.coverImage}
-              date={heroPost.date}
-              author={heroPost.author}
-              slug={heroPost.slug}
-              excerpt={heroPost.excerpt}
-            />
-          )}
-          {morePosts.length > 0 && <MoreStories posts={morePosts} />}
-        </Container>
-      </Layout>
-    </>
+    <ContainerBlock customMeta={{ title: 'Blog - MJ Linane' }}>
+      <Blog allPosts={allPosts} />
+    </ContainerBlock>
   )
 }
 
