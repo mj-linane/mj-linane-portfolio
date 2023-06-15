6b06a3cec367bd3c6566c3c987ec2da324df4578
MJ Linane <19750620+mj-linane@users.noreply.github.com>
2023-06-14 21:07:28 -0400

    fix type information for function

--- lib/api.ts
+++ lib/api.ts
@@ -8,32 +8,34 @@ export function getPostSlugs() {
   return fs.readdirSync(postsDirectory)
 }
 
-type PostData = Omit<PostType, 'slug' | 'content'>
-
-export function getPostBySlug(slug: string, fields: string[] = []): PostType {
+export function getPostBySlug(slug: string, fields: string[] = []) {
   const realSlug = slug.replace(/\.md$/, '')
   const fullPath = join(postsDirectory, `${realSlug}.md`)
   const fileContents = fs.readFileSync(fullPath, 'utf8')
   const { data, content } = matter(fileContents)
 
-  const postData = data as PostData
+  type Items = Record<string, string>
 
-  const post: PostType = {
-    slug: realSlug,
-    content,
-    ...postData,
-  }
+  const items: Items = {}
+
   // Ensure only the minimal needed data is exposed
-  const items: Partial<PostType> = {} // Use Partial<Post> as not all properties of Post may be present
-  fields.forEach((field: keyof PostType) => {
-    if (post[field] !== undefined) {
-      items[field] = post[field]
+  fields.forEach((field) => {
+    if (field === 'slug') {
+      items[field] = realSlug
+    }
+    if (field === 'content') {
+      items[field] = content
+    }
+
+    if (typeof data[field] !== 'undefined') {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      items[field] = data[field]
     }
   })
-  return items as PostType
+  return items
 }
 
-export async function getAllPosts(fields: string[] = []): Promise<PostType[]> {
+export async function getAllPosts(fields: string[] = []) {
   const slugs = getPostSlugs()
   const posts = await Promise.all(
     slugs.map((slug) => getPostBySlug(slug, fields))
